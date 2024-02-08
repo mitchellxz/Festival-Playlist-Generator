24ba612126100014b76b59037a2160033dfd1602
mitchell <72942268+mitchellxz@users.noreply.github.com>
2024-02-08 00:46:23 -0500

    Add files via upload

---
+++ FestivalPlaylistGenerator.py
@@ -0,0 +1,168 @@
+from dotenv import load_dotenv
+import os
+import base64
+import requests
+from requests import get, post
+import json
+from flask import Flask, redirect, request, jsonify, session
+import urllib.parse
+from datetime import datetime, timedelta
+
+load_dotenv()
+
+
+app = Flask(__name__)
+app.secret_key = '3rj4j5j6-3j4j5j-222j-2350293jjj'
+
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+redirect_uri = os.getenv("REDIRECT_URI")
+auth_url = os.getenv("AUTH_URL")
+token_url = os.getenv("TOKEN_URL")
+api_base_url = os.getenv("API_BASE_URL")
+
+@app.route('/')
+def index():
+    return "Welcome to my Spotify App <a href='/login'>Login with Spotify</a>"
+
+@app.route('/login')
+def login():
+    scope = 'user-read-private user-read-email'
+
+    params = {
+        'client_id': client_id,
+        'response_type': 'code',
+        'scope': scope,
+        'redirect_uri': redirect_uri,
+        'show_dialog': True
+    }
+
+    new_auth_url = f"{auth_url}?{urllib.parse.urlencode(params)}"
+
+    return redirect(new_auth_url)
+
+@app.route('/callback')
+def callback():
+    if 'error' in request.args:
+        return jsonify({"error": request.args['error']})
+    
+    if 'code' in request.args:
+        req_body = {
+            'code': request.args['code'],
+            'grant_type': 'authorization_code',
+            'redirect_uri': redirect_uri,
+            'client_id': client_id,
+            'client_secret': client_secret
+        }
+
+        response = requests.post(token_url, data=req_body)
+        token_info = response.json()
+
+        session['access_token'] = token_info['access_token']
+        session['refresh_token'] =token_info['refresh_token']
+        session['expires_at'] = datetime.now().timestamp() + token_info['expires_in']
+
+        return redirect('/playlists')
+    
+@app.route('/playlists')
+def get_playlists():
+    if 'access_token' not in session:
+        return redirect('/login')
+    
+    if datetime.now().timestamp() > session['expires_at']:
+        return redirect('/refresh-token')
+    
+    headers = {
+        'Authorization': f"Bearer {session['access_token']}"
+    }
+
+    response  =requests.get(api_base_url + 'me/playlists', headers=headers)
+    playlists = response.json()
+
+    return jsonify(playlists)
+
+@app.route('/refresh-token')
+def refresh_token():
+    if 'refresh_token' not in session:
+        return redirect('/login')
+    
+    if datetime.now().timestamp() > session['expires_at']:
+        req_body = {
+            'grant_type': 'refresh_token',
+            'refresh_token': session['refresh_token'],
+            'client_id': client_id,
+            'client_secret': client_secret
+        }
+
+        response = requests.post(token_url, data=req_body)
+        new_token_info = response.json()
+        
+        session['access_token'] = new_token_info['access_token']
+        session['expires_at'] = datetime.now().timestamp() + new_token_info['expires_in']
+
+        return redirect('/playlists')
+
+def get_token():
+    auth_string = client_id + ":" + client_secret
+    auth_bytes = auth_string.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic " + auth_base64,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {"grant_type": "client_credentials"}
+    result = post(url, headers=headers, data=data)
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+    return token
+
+def get_auth_header(token):
+    return {"Authorization": "Bearer " + token}
+
+def search_for_artist(token, artist_name):
+    url = "https://api.spotify.com/v1/search"
+    headers = get_auth_header(token)
+    query = f"?q={artist_name}&type=artist&limit=1"
+
+    query_url = url + query
+    result = get(query_url, headers=headers)
+    json_result = json.loads(result.content)["artists"]["items"]
+
+    if len(json_result) == 0:
+        print("Artist not found.")
+        return None
+    
+    return json_result[0]
+
+def get_songs_by_artist(token, artist_id):
+    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
+    headers = get_auth_header(token)
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)["tracks"]
+    return json_result
+
+
+def get_user_id(token):
+    url = "https://api.spotify.com/v1/me"
+    headers = get_auth_header(token)
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)
+    return json_result
+
+
+token = get_token()
+result = search_for_artist(token, "calvin harris")
+artist_id = result["id"]
+songs = get_songs_by_artist(token, artist_id)
+
+for idx, song in enumerate(songs):
+    print(f"{idx + 1}. {song['external_urls']}")
+
+user_id = get_user_id(token)
+
+print(user_id)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
