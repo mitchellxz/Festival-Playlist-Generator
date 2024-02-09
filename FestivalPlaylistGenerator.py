6265d9b2f8c3b0d32983f8955e26299d0cd8a445
mitchell <72942268+mitchellxz@users.noreply.github.com>
2024-02-09 17:24:11 -0500

    Update FestivalPlaylistGenerator.py

--- FestivalPlaylistGenerator.py
+++ FestivalPlaylistGenerator.py
@@ -1,174 +1,92 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 import os
-import base64
-import requests
-from requests import get, post
+import spotipy.util as util
+import webbrowser
+import sys
 import json
-from flask import Flask, redirect, request, jsonify, session
-import urllib.parse
 from datetime import datetime, timedelta
 
 load_dotenv()
 
-""" 
-REDIRECT_URI = "http://localhost:5000/callback"
-AUTH_URL = "https://accounts.spotify.com/authorize"
-TOKEN_URL = "https://accounts.spotify.com/api/token"
-API_BASE_URL = "https://api.spotify.com/v1/" 
-"""
-
-app = Flask(__name__)
-app.secret_key = '3rj4j5j6-3j4j5j-222j-2350293jjj'
-
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 redirect_uri = os.getenv("REDIRECT_URI")
-auth_url = os.getenv("AUTH_URL")
-token_url = os.getenv("TOKEN_URL")
-api_base_url = os.getenv("API_BASE_URL")
 
-@app.route('/')
-def index():
-    return "Welcome to my Spotify App <a href='/login'>Login with Spotify</a>"
 
-@app.route('/login')
-def login():
-    scope = 'user-read-private user-read-email'
 
-    params = {
-        'client_id': client_id,
-        'response_type': 'code',
-        'scope': scope,
-        'redirect_uri': redirect_uri,
-        'show_dialog': True
-    }
-
-    new_auth_url = f"{auth_url}?{urllib.parse.urlencode(params)}"
-
-    return redirect(new_auth_url)
-
-@app.route('/callback')
-def callback():
-    if 'error' in request.args:
-        return jsonify({"error": request.args['error']})
+def get_user_token():
+    scope = "playlist-modify-public"
+    try:
+        token = util.prompt_for_user_token(username, scope)
+    except:
+        os.remove(f".cache-{username}")
+        token = util.prompt_for_user_token(username, scope)
     
-    if 'code' in request.args:
-        req_body = {
-            'code': request.args['code'],
-            'grant_type': 'authorization_code',
-            'redirect_uri': redirect_uri,
-            'client_id': client_id,
-            'client_secret': client_secret
-        }
-
-        response = requests.post(token_url, data=req_body)
-        token_info = response.json()
-
-        session['access_token'] = token_info['access_token']
-        session['refresh_token'] =token_info['refresh_token']
-        session['expires_at'] = datetime.now().timestamp() + token_info['expires_in']
-
-        return redirect('/playlists')
+    user_token = spotipy.Spotify(auth=token)
     
-@app.route('/playlists')
-def get_playlists():
-    if 'access_token' not in session:
-        return redirect('/login')
+    return user_token
+
+
+def get_user_info(user_token):
+    user = user_token.current_user()
+
+    user_id = user['id']
+
+    return user_id
+
+
+def get_artist_id(user_token, artist):
+    query = artist
+    results = user_token.search(query, 1, 0, "artist")
+
+    artist_id = results['artists']['items'][0]['id']
+
+    return artist_id
     
-    if datetime.now().timestamp() > session['expires_at']:
-        return redirect('/refresh-token')
+
+def get_artist_top_songs(user_token, artist_id):
+    results = user_token.artist_top_tracks(artist_id, country='US')
+
+    url_list = []
+
+    for track in results['tracks'][:10]:
+        print('track    : ' + track['name'])
+        url_list.append(track['uri'])
     
-    headers = {
-        'Authorization': f"Bearer {session['access_token']}"
-    }
-
-    response  =requests.get(api_base_url + 'me/playlists', headers=headers)
-    playlists = response.json()
-
-    return jsonify(playlists)
-
-@app.route('/refresh-token')
-def refresh_token():
-    if 'refresh_token' not in session:
-        return redirect('/login')
-    
-    if datetime.now().timestamp() > session['expires_at']:
-        req_body = {
-            'grant_type': 'refresh_token',
-            'refresh_token': session['refresh_token'],
-            'client_id': client_id,
-            'client_secret': client_secret
-        }
-
-        response = requests.post(token_url, data=req_body)
-        new_token_info = response.json()
-        
-        session['access_token'] = new_token_info['access_token']
-        session['expires_at'] = datetime.now().timestamp() + new_token_info['expires_in']
-
-        return redirect('/playlists')
-
-def get_token():
-    auth_string = client_id + ":" + client_secret
-    auth_bytes = auth_string.encode("utf-8")
-    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
-
-    url = "https://accounts.spotify.com/api/token"
-    headers = {
-        "Authorization": "Basic " + auth_base64,
-        "Content-Type": "application/x-www-form-urlencoded"
-    }
-    data = {"grant_type": "client_credentials"}
-    result = post(url, headers=headers, data=data)
-    json_result = json.loads(result.content)
-    token = json_result["access_token"]
-    return token
-
-def get_auth_header(token):
-    return {"Authorization": "Bearer " + token}
-
-def search_for_artist(token, artist_name):
-    url = "https://api.spotify.com/v1/search"
-    headers = get_auth_header(token)
-    query = f"?q={artist_name}&type=artist&limit=1"
-
-    query_url = url + query
-    result = get(query_url, headers=headers)
-    json_result = json.loads(result.content)["artists"]["items"]
-
-    if len(json_result) == 0:
-        print("Artist not found.")
-        return None
-    
-    return json_result[0]
-
-def get_songs_by_artist(token, artist_id):
-    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
-    headers = get_auth_header(token)
-    result = get(url, headers=headers)
-    json_result = json.loads(result.content)["tracks"]
-    return json_result
+    return url_list
 
 
-def get_user_id(token):
-    url = "https://api.spotify.com/v1/me"
-    headers = get_auth_header(token)
-    result = get(url, headers=headers)
-    json_result = json.loads(result.content)
-    return json_result
+def create_playlist(user_token, playlist_name):
+
+    results = user_token.user_playlist_create(user_id, playlist_name, public=True, collaborative=False, description="")
+    print(json.dumps(results, sort_keys=True, indent=4))
+
+    return results['id']
 
 
-token = get_token()
-result = search_for_artist(token, "calvin harris")
-artist_id = result["id"]
-songs = get_songs_by_artist(token, artist_id)
+def add_songs_to_playlist(user_token, user_id, playlist_id, tracks):
+    user_token.user_playlist_add_tracks(user_id, playlist_id, tracks, position=None)
 
-for idx, song in enumerate(songs):
-    print(f"{idx + 1}. {song['external_urls']}")
+formatted_datetime = datetime.now().strftime("%Y%m%d%H%M%S%f")
 
-user_id = get_user_id(token)
+username = "user-" + formatted_datetime
 
-print(user_id)
+user_token = get_user_token()
+user_id = get_user_info(user_token)
+playlist_id = create_playlist(user_token, "new playlist")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+artist_list = ["", ""]
+full_url_list = []
+
+for artist in artist_list:
+    artist_id = get_artist_id(user_token, artist)
+    url_list = get_artist_top_songs(user_token, artist_id)
+
+    full_url_list.append(url_list)
+
+
+for url_list in full_url_list:
+    add_songs_to_playlist(user_token, user_id, playlist_id, url_list)
